b0f41c75025a86f17023a4c8b1a6ea70b363bcce
Serge Inacio <sergeinacio@gmail.com>
2017-04-17 21:48:33 +0200

    added custom type case studies and styled it

---
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying case studies
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<article class="case-study">
+					<?php the_content(); ?>
+				</article>
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
