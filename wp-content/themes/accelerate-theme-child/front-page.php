b87f4fc267632edc53aa3e86f15cd9a8c04079bc
Serge Inacio <sergeinacio@gmail.com>
2017-04-19 23:00:41 +0200

    Added case studies to home page and styled it

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -22,21 +22,53 @@ get_header(); ?>
 				<a class="button" href="<?php echo home_url(); ?>/blog">View Our Work</a>
 			</div>
 		<?php endwhile; // end of the loop. ?>
-	</div><!-- .container -->
+	</div><!-- .site-content -->
 </section><!-- .home-page -->
+
+<section class="featured-work">
+	<div class="site-content">
+
+		<h4>Featured Work</h4>
+
+		<ul class="homepage-featured-work">
+		<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+			
+			<?php while ( have_posts() ) : the_post();
+				$image_1 	= get_field('image_1');
+				$size 		= "medium";
+			?>
+			<li class="individual-feature-work">
+				<figure>
+					<?php if($image_1) {
+						echo wp_get_attachment_image( $image_1, $size );
+					} ?>
+				</figure>
+
+				<h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+
+				<?php endwhile; ?>
+			</li>
+		<?php wp_reset_query(); ?>
+		</ul>
+
+	</div><!-- .site-conten -->
+</section><!-- .featured-work -->
+
 <section class="recent-posts">
 	<div class="site-content">
 		<div class="blog-post">
+
 		<h4>From the Blog</h4>
 		<?php query_posts('posts_per_page=1'); ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-		<h2><?php the_title(); ?></h2>
-		<?php the_excerpt(); ?> 
-		<a class="read-more-link" href="<?php the_permalink(); ?>">Read More <span>&rsaquo;</span></a>
-		<?php endwhile; ?> 
+			<?php while ( have_posts() ) : the_post(); ?>
+				<h2><?php the_title(); ?></h2>
+				<?php the_excerpt(); ?> 
+				<a class="read-more-link" href="<?php the_permalink(); ?>">Read More <span>&rsaquo;</span></a>
+			<?php endwhile; ?> 
 		<?php wp_reset_query(); ?>
-		</div>
-	</div>
-</section>
+
+		</div><!-- .blog-post -->
+	</div><!-- .site-content -->
+</section><!-- .recent-posts -->
 
 <?php get_footer(); ?>
